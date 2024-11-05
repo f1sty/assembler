d198d4a87a8bfda041ca7e3e533b89fe3fb37e14
Yurii Skrynnykov <truef1s7@gmail.com>
2024-11-05 20:43:14 +0200

    Update sandbox

--- sandbox/sandbox.asm
+++ sandbox/sandbox.asm
@@ -1,10 +1,17 @@
 section .data
+sum: dq 0
 section .bss
 
 section .text
 global _start
 _start:
   nop
+
+  mov rcx, 18
+.loop:
+  add qword [sum], 3
+  loop .loop
+
   mov rax, 250
   blsr rbx, rax
   blsr rax, rbx
